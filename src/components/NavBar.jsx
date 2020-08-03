3ea6839d04309a0f2098341d1c99cf91e3f33907
haikalazhar197 <haikalazhar197@gmail.com>
2020-08-04 01:55:41 +0800

    Fixed Registration page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,22 +2,55 @@ import React from "react";
 
 import { Navbar, Nav, Button } from "react-bootstrap";
 
-const NavBar = () => {
-  return (
-    <div>
-      <Navbar bg="dark" expand="lg" variant="dark">
-        <Navbar.Brand href="#home">CiTRA IIUM Membership</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-          </Nav>
-          <Button className="mr-sm-2">Register</Button>
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
-  );
+const NavBar = ({ headerType = "landing" }) => {
+  if (headerType === "landing") {
+    return (
+      <div>
+        <Navbar bg="dark" expand="lg" variant="dark">
+          <Navbar.Brand href="#home">CiTRA IIUM Membership</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+            <Button className="mr-sm-2">Register</Button>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
+  } else if (headerType === "loggedin") {
+    return (
+      <div>
+        <Navbar bg="dark" expand="lg" variant="dark">
+          <Navbar.Brand href="#home">CiTRA IIUM Membership</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+            <Button className="mr-sm-2">Logout</Button>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Navbar bg="dark" expand="lg" variant="dark">
+          <Navbar.Brand href="#home">CiTRA IIUM Membership</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 };
 
 export default NavBar;
--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,92 +1,130 @@
 import React from "react";
 
+import { Container, Form, Button } from "react-bootstrap";
+
+import NavBar from "../components/NavBar";
+
 const Registration = () => {
   return (
-    <div>
-       <div className = "container">
-        <form>
-        <div class="form-group">
-          <label for="name">Name</label>
-          <input type="text" class="form-control" id="name" />
-        </div>
+    <div className="page">
+      <NavBar headerType="registration" />
+      <Container className="mt-5">
+        <Form>
+          <Form.Group>
+            <Form.Label for="name">Name</Form.Label>
+            <Form.Control type="text" id="name" />
+          </Form.Group>
 
-        <div class="form-group">
-          <label for="age">Age</label>
-          <input type="text" class="form-control" id="age" />
-        </div>
+          <Form.Group>
+            <Form.Label for="age">Age</Form.Label>
+            <Form.Control type="text" id="age" inputMode="numeric" />
+          </Form.Group>
 
-        <div class="form-group">
-          <label for="matricNo">Matric Number</label>
-          <input type="text" class="form-control" id="matricNo" />
-        </div>
-        
-        <label for="kulliyah">Kulliyah</label>
-        <select class="form-control">
-          <option>Please select your Kulliyah</option>
-          <option>KOE</option>
-          <option>KICT</option>
-          <option>KOED</option>
-          <option>IRKHS</option>
-          <option>KENMS</option>
-          <option>AIKOL</option>
-        </select>
+          <Form.Group>
+            <Form.Label for="matricNo">Matric Number</Form.Label>
+            <Form.Control type="text" id="matricNo" inputMode="numeric" />
+          </Form.Group>
 
-        <label for="course">Course</label>
-        <select class="form-control">
-          <option>Please select your course</option>
-          <option>BCS</option>
-          <option>BIT</option>
-          <option>MECHATRONIC</option>
-          <option>MECHA</option>
-          <option>MANU</option>
-          <option>SYARIAH</option>
-        </select>
+          <Form.Group>
+            <Form.Label for="kulliyah">Kulliyah</Form.Label>
+            <Form.Control as="select">
+              <option>Please select your Kulliyah</option>
+              <option>KOE</option>
+              <option>KICT</option>
+              <option>KOED</option>
+              <option>IRKHS</option>
+              <option>KENMS</option>
+              <option>AIKOL</option>
+            </Form.Control>
+          </Form.Group>
 
-        <div class="form-group">
-          <label for="intru">Instrument</label>
-          <input type="text" class="form-control" id="instru" />
-        </div>
+          <Form.Group>
+            <Form.Label for="course">Course</Form.Label>
+            <Form.Control as="select">
+              <option>Please select your course</option>
+              <option>BCS</option>
+              <option>BIT</option>
+              <option>MECHATRONIC</option>
+              <option>MECHA</option>
+              <option>MANU</option>
+              <option>SYARIAH</option>
+            </Form.Control>
+          </Form.Group>
 
-        <div class = "form-group position">
-          <label for="position" class = "border-bottom">Position</label>
-          <br/>
-          <input class="form-check-input " type="radio" name="pres" id="position" value="option1"/>
-          <label class="form-check-label " for="1">President</label>
-        </div>
-        <div class = "form-group position">
-          <input class="form-check-input" type="radio" name="vp1" id="position" value="option2"/>
-          <label class="form-check-label" for="2">Vice President 1</label>
-        </div> 
-        <div class = "form-group position">
-          <input class="form-check-input" type="radio" name="member" id="position" value="option3" checked/>
-          <label class="form-check-label" for="3">Members</label>
-        </div>
+          <Form.Group>
+            <Form.Label for="intru">Instrument</Form.Label>
+            <Form.Control type="text" id="instru" />
+          </Form.Group>
 
-        <label for="club">Club</label>
-        <select class="form-control">
-          <option>Please select a club</option>
-          <option>IAB</option>
-          <option>GGM</option>
-          <option>ANDEKA</option>
-          <option>ANGKLUNG</option>
-          <option>NAFASTARI</option>
-        </select>
+          {/* <Form.Group className="position">
+            <Form.Label for="position" className="border-bottom">
+              Position
+            </Form.Label>
+            <br />
+            <input
+              className="form-check-input "
+              type="radio"
+              name="pres"
+              id="position"
+              value="option1"
+            />
+            <label className="form-check-label " for="1">
+              President
+            </label>
+          </Form.Group> */}
+          {/* <div className="form-group position">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="vp1"
+              id="position"
+              value="option2"
+            />
+            <label className="form-check-label" for="2">
+              Vice President 1
+            </label>
+          </div> */}
+          {/* <div className="form-group position">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="member"
+              id="position"
+              value="option3"
+              checked
+            />
+            <label className="form-check-label" for="3">
+              Members
+            </label>
+          </div> */}
 
-        <div class="form-group">
-          <label for="email">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-        </div>
+          <Form.Group>
+            <label for="club">Club</label>
+            <select className="form-control">
+              <option>Please select a club</option>
+              <option>IAB</option>
+              <option>GGM</option>
+              <option>ANDEKA</option>
+              <option>ANGKLUNG</option>
+              <option>NAFASTARI</option>
+            </select>
+          </Form.Group>
 
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1"/>
-        </div>
+          <Form.Group>
+            <Form.Label for="email">Email address</Form.Label>
+            <Form.Control
+              type="email"
+              id="exampleInputEmail1"
+              ariaDescribedby="emailHelp"
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
 
-
-        <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-      </div>
+          <Button type="submit">Submit</Button>
+        </Form>
+      </Container>
     </div>
   );
 };
