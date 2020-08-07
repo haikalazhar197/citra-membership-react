a121b1878cef29466eef7de86a675545f5a34ba6
Samsul Adry <samsuladry@gmail.com>
2020-08-07 17:28:59 +0800

    modify 3

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,14 +1,150 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Container, Form, Button } from "react-bootstrap";
+import { Container, Form, Button, ListGroup } from "react-bootstrap";
 
 import NavBar from "../components/NavBar";
 
 const Registration = () => {
+  
+  const [detail, setDetail] = useState({
+    name: '',
+    matricNo: '',
+    age: '',
+    kulliyah: '',
+    course: '',
+    club: '',
+    email : '',
+  });
+
+  // function submit() {
+  //   console.log(detail)
+  // }
+
+  const submit = e => {
+    e.preventDefault();
+    console.log(detail);
+    setDetail({
+    name: '',
+    matricNo: '',
+    age: '',
+    kulliyah: '',
+    course: '',
+    club: '',
+    email : '',
+    });
+  }
+
   return (
     <div className="page">
       <NavBar headerType="registration" />
-      <Container className="mt-5">
+        <div className='container mt-5'>
+          <ListGroup as="ul">
+            <ListGroup.Item as="li" active>
+              Registration
+            </ListGroup.Item>
+            <ListGroup.Item as="li">
+            <Form>
+              
+              <Form.Group controlId="name">
+                  <Form.Label>Full Name</Form.Label>
+                  <Form.Control 
+                  type="text" 
+                  value = {detail.name}
+                  placeholder="Enter full Name"
+                  onChange={e => setDetail({ ... detail, name: e.target.value })} />
+              </Form.Group>
+
+              <Form.Group controlId="matricNo">
+                  <Form.Label>Matric Number</Form.Label>
+                  <Form.Control 
+                  type="number" 
+                  placeholder="Enter matric Number"
+                  value = {detail.matricNo} 
+                  onChange={e => setDetail({... detail, matricNo: e.target.value })} />
+              </Form.Group>
+
+              <Form.Group controlId="age">
+                  <Form.Label>Age</Form.Label>
+                  <Form.Control 
+                  type="number" 
+                  placeholder="Enter age"
+                  value = {detail.age} 
+                  onChange={e => setDetail({... detail, age: e.target.value })} />
+              </Form.Group>
+
+              <Form.Group controlId="kulliyah">
+                  <Form.Label>Kulliyah</Form.Label>
+                  <Form.Control 
+                  type="text" 
+                  placeholder="Enter Kulliyah"
+                  value = {detail.kulliyah} 
+                  onChange={e => setDetail({... detail, kulliyah: e.target.value })} />
+              </Form.Group>
+
+              <Form.Group controlId="course">
+                  <Form.Label>Course</Form.Label>
+                  <Form.Control 
+                  type="text" 
+                  placeholder="Enter Course"
+                  value = {detail.course} 
+                  onChange={e => setDetail({... detail, course: e.target.value })} />
+              </Form.Group>
+
+              <Form.Group controlId="club">
+                <Form.Label>Club</Form.Label>
+                  <Form.Control as="select" 
+                  type= "select"
+                  value = {detail.club} 
+                  onChange={e => setDetail({... detail, club: e.target.value })}>
+                    <option>Club: </option>
+                    <option>IIUM Acoustic Band</option>
+                    <option>IIUM Gamelan Gema Gangsa</option>
+                    <option>IIUM Andeka Caklempong</option>
+                    <option>IIUM Angklung</option>
+                    <option>IIUM Nafas Tari</option>
+                </Form.Control>
+              </Form.Group>
+
+
+              <Form.Group controlId="designation">
+                  <Form.Label>Instrument</Form.Label>
+                  <Form.Control 
+                  type="text" 
+                  placeholder="Instrument"
+                  value = {detail.designation} 
+                  onChange={e => setDetail({... detail, designation: e.target.value })} />
+              </Form.Group>
+
+
+              <Form.Group controlId="email">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control 
+                    type="email" 
+                    placeholder="Enter email"
+                    value = {detail.email} 
+                    onChange={e => setDetail({... detail, email: e.target.value })} />
+                </Form.Group>
+
+                <Button 
+                    variant="primary"  
+                    size="lg" 
+                    block
+                    onClick= { e=> submit(e) }>
+                    Submit
+                </Button>
+              </Form> 
+            </ListGroup.Item>
+        </ListGroup>
+      </div>
+    </div>
+  );
+};
+
+export default Registration;
+
+
+
+{/* <Container className="mt-5">
         <Form>
           <Form.Group>
             <Form.Label for="name">Name</Form.Label>
@@ -96,7 +232,7 @@ const Registration = () => {
             <label className="form-check-label" for="3">
               Members
             </label>
-          </div> */}
+          </div> 
 
           <Form.Group>
             <label for="club">Club</label>
@@ -124,9 +260,4 @@ const Registration = () => {
 
           <Button type="submit">Submit</Button>
         </Form>
-      </Container>
-    </div>
-  );
-};
-
-export default Registration;
+      </Container> */}
